d3cd7c6aca5562967927a07d03e53ed3099acbaa
nobodyzxc <dici86.chen@gmail.com>
2018-05-07 00:43:25 +0800

    try ok

--- seproj/routes/index.js
+++ seproj/routes/index.js
@@ -9,11 +9,44 @@ router.get('/', function(req, res, next) {
 });
 
 /* simple database sample page */
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://[act]:[pw]@ds161539.mlab.com:61539/se-project-2018");
+//stuff pw
+
+console.log(mongoose.connection.readyState);
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema(
+  {
+    inst:      { type: String },
+    point:     { type: Number },
+    time:      { type: String },
+    url:       { type: String},
+    cname:     { type: String},
+  }
+);
+
+function reqName(name, callback){
+  var result;
+  var Course = mongoose.model('Course', UserSchema, 'test');
+  Course.find({inst:name})
+    .select('inst cname point time')
+    .exec(function(err, courses){
+      callback(courses);
+    });
+}
+
 router.get('/test.html', function (req, res) {
-  //console.log(req);
-  //console.log(res);
-  res.render('test', { title: 'Express' });
-  //res.send('Fuck World!');
+  console.log(req.query);
+  if(req.query.name === undefined)
+    res.render('test', {title: 'REQ COURSE', data:[]});
+  else{
+    reqName(req.query.name, function(result){
+      res.render('test', {title: 'REQ COURSE', data:result});
+    });
+  }
+  //class: reqName(req.query.name)
 });
 
 module.exports = router;
